30f39550b27ea55cbe68789e989f68d2a3f7cd6f
LSz <46381400+LukasSchz@users.noreply.github.com>
2019-03-14 16:47:13 +0100

    Update BattleGUI.java

--- javaGame/BattleGUI.java
+++ javaGame/BattleGUI.java
@@ -9,8 +9,8 @@ import java.awt.event.ActionListener;
 /**
  * Die GUI fuer das Kampfsystem
  *
- * @author Artur Sarkisjan 4571377 Gruppe 10b
- * @author Lukas Schulz 4571351 Gruppe 10b
+ * @author Artur Sarkisjan
+ * @author Lukas Schulz
  *
  * @version 1.0
  */
