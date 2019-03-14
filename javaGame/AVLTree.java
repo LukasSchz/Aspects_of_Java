5f18fb109afeb2542463937c89f592c7bd2ab6d0
LSz <46381400+LukasSchz@users.noreply.github.com>
2019-03-14 16:46:37 +0100

    Update AVLTree.java

--- javaGame/AVLTree.java
+++ javaGame/AVLTree.java
@@ -7,8 +7,8 @@ import java.io.Serializable;
  *
  * @param <T> Typ welcher Comparable implementiert
  *
- * @author Artur Sarkisjan 4571377 Gruppe 10b
- * @author Lukas Schulz 4571351 Gruppe 10b
+ * @author Artur Sarkisjan
+ * @author Lukas Schulz
  *
  * @version 1.0
 */
